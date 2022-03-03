bece1d5dc64f2bdb180c43e97150d9fd6d3ec245
Adrien VRD <adrien.vaucard@gmail.com>
2022-03-03 08:46:54 +0100

    Miam

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 let kebab = {
     ingredients: [
-        "lamb",
         "salad",
         "tomato",
+        "onions",
         "white sauce"
     ]
 }
+
+let meats = [
+    "lamb",
+    "egg",
+];
+
+let pessis = [
+    "fish",
+    "shrimp"
+]
+
+let isVegan = true;
+let isPessi = true;
+
+meats.every(element => {
+    if (kebab.ingredients.includes(element)) {
+        isVegan = false;
+    }
+});
+
+if (isVegan) {
+    console.log("Kebab is vegan");
+} else {
+    console.log("Kebab is not vegan");
+}
+
+pessis.every(element => {
+    if (kebab.ingredients.includes(element)) {
+        pessis = false;
+    }
+});
+
+if (isPessi) {
+    console.log("Kebab is pessi");
+} else {
+    console.log("Kebab is not pessi");
+}
+
+
+
+  
